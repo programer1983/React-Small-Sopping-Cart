c759d7bd4148b657d55afc922f6fee2e19a0e59a
programer1983 <programer972@gmail.com>
2022-03-30 12:59:52 +0300

    commit-8;

--- src/context.js
+++ src/context.js
@@ -1,16 +1,23 @@
 import {useState, useReducer, useContext, useEffect, createContext} from "react"
 import CartItems from "./data"
-// import reducer from "./reducer"
+import reducer from "./reducer"
 
 const AppContext = createContext()
 
+const initialState = {
+    loading: false,
+    cart: CartItems,
+    total: 0,
+    amount: 0,
+}
+
 const AppProvider = ({children}) => {
-    const [cart, setCart] = useState(CartItems)
+    const [state, dispatch] = useReducer(reducer, initialState)
 
     return (
         <AppContext.Provider
            value = {{
-            cart,
+            ...state
            }}
         >
             {children}
